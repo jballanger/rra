763d7d7d03ab87d362573b6a2891eb163713b642
jballanger <ballanger.julien@gmail.com>
2019-01-30 11:00:19 +0100

    Updates

---
+++ precache-manifest.a94a2a16109791082e4127e3943780e4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "28b78eb90676ba6ef0ad",
+    "url": "/rra/static/js/runtime~main.28b78eb9.js"
+  },
+  {
+    "revision": "19383a7f38c67288ecb0",
+    "url": "/rra/static/js/main.19383a7f.chunk.js"
+  },
+  {
+    "revision": "452d606005471eceb989",
+    "url": "/rra/static/js/1.452d6060.chunk.js"
+  },
+  {
+    "revision": "a2990c4230ec0dd0a7c60b05098bb8eb",
+    "url": "/rra/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/rra/precache-manifest.a94a2a16109791082e4127e3943780e4.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/rra/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
