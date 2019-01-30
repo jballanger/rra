aa34e315e009fd95c1718b490efae66294fff8c9
jballanger <ballanger.julien@gmail.com>
2019-01-30 10:46:17 +0100

    Update App component to use routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import TodoList from './components/TodoList';
-import Counter from './components/Counter';
+import { withRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import Routes from './routes';
+
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    @import url('https://fonts.googleapis.com/css?family=Sarabun');
+    font-family: 'Sarabun', sans-serif;
+    background-color: #DDDDDD;
+  }
+  .container {
+    text-align: center;
+  }
+`;
 
 class App extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
-        <p>React Redux Application</p>
-        <Counter />
-        <br></br>
-        <TodoList />
+        <GlobalStyles />
+        {history.location.pathname !== '/'
+          ? <button onClick={history.goBack}>Back</button>
+          : ''
+        }
+        <div className="container">
+          <p>React Redux Application</p>
+          <Routes />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
