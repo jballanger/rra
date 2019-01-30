d50048a5f9a89a12210da953cad793856afb806d
jballanger <ballanger.julien@gmail.com>
2019-01-30 10:45:54 +0100

    Add router and history to the app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Router } from 'react-router';
 import { Provider } from 'react-redux';
+import { history } from './helpers/history';
 import store from './store';
-
 import App from './App';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>,
   rootElement
 );
