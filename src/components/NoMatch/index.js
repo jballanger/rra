804a1ed947bc17c2b917127af330a0a6890b122b
jballanger <ballanger.julien@gmail.com>
2019-01-30 10:44:56 +0100

    Add NoMatch component

---
+++ src/components/NoMatch/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { MagicText } from './style';
+
+class NoMatch extends Component {
+  render() {
+    return <MagicText>404</MagicText>;
+  }
+}
+
+export default NoMatch;
