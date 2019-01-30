01a5ed0b6ee5a98f57293b52b64549f4b1cda79f
jballanger <ballanger.julien@gmail.com>
2019-01-30 17:54:55 +0100

    Apply thunk middleware to store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import reducer from './reducers';
 
-export default createStore(reducer, applyMiddleware(logger));
+export default createStore(reducer, applyMiddleware(thunk, logger));
